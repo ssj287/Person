e1254de9f0b55dd5255afc4270119bf6a7032ac1
qianyu <1158537287@qq.com>
2017-12-26 10:00:48 +0800

    no

---
+++ 换行符以及列表.py
@@ -0,0 +1,13 @@
+tabby_cat="\tI'm tabbled in"#\t空四格
+persian_cat="I'm split\non a line"#\n换行即使在单词内
+backslash_cat="I'm \\ a \\ cat"#\\代表分割符显示为\
+fat_cat="""
+I'll do a list
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
