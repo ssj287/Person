6460742459825fac801902d9d6854e09c15602fd
qianyu <1158537287@qq.com>
2017-12-26 09:54:31 +0800

    no

---
+++ 多行文本输出.py
@@ -0,0 +1,10 @@
+print("""
+saddadadasdasd
+asdadad
+sadsadad
+asdasdsa
+sdadsadsad
+""")#"""输出多行
+print("a\na\ndfsf")#\n换行
+tabby_cat="\tI'm tabbled in"#\t空四格
+persian_cat="I'm split\non a line"#\n换行即使在单词内
